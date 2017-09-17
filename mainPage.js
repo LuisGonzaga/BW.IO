8347ad5e847c7aa91f95880ee68c1a50bfc7b082
LuisGonzaga <luis.caldas.costa@gmail.com>
2017-09-17 15:54:51 +0100

    updt

--- mainPage.js
+++ mainPage.js
@@ -1,44 +1,28 @@
 <script type="text/javascript">
+var Log = function(dataLog){
+	var d = new Date();
+	logFile.push(d.toUTCString()+dataLog);
+}
 
-var logFile = [d.toUTCString()+" Start"];
+// 	function Timer(arg) {
+// 		var copy = [];
+// 		arg.logFile.forEach(function(item){
+// 		copy.push(item)
+// 		});
+// 		console.log(copy);
+//   }
 var $counter = 0;
-var copy = [];
-var d = new Date();
-var pageCoords = "0,0";
-
-$( document ).ready(function() {
-	
-	setTimeout(Timer, 500, logFile)
-  
-//var querystring = require('querystring');
-
-	
-});
-
-$("div").mousemove(function( event ) {
-	  pageCoords = "( " + event.pageX + ", " + event.pageY + " )";
-	  $( "span" ).text( "(X, Y) : " + pageCoords );
-	  logFile.push(d.toUTCString()+"(X, Y) : " + pageCoords);
-	  
-});	
-			
 $("#target").mousedown(function() {
 		  $counter++;
 		  $("p").text(" Clicks: "+$counter);
 		  Log("Clicks " + $counter);
 });
 
-var Log = function(dataLog){
-	d = new Date();
-	logFile.push(d.toUTCString()+dataLog);
-}
-
-function Timer(arg) {
-	
-	arg.logFile.forEach(function(item){
-	copy.push(item)
-	});
-	console.log(copy);
- }
- </script>
+$("div").mousemove(function( event ) {
+	  var pageCoords = "( " + event.pageX + ", " + event.pageY + " )";
+	  $( "span" ).text( "(X, Y) : " + pageCoords );
+	  //logFile.push(d.toUTCString()+"(X, Y) : " + pageCoords);
+	  
+});	
+</script>
   
--- node/getEndpoint.js
+++ node/getEndpoint.js
@@ -2,16 +2,27 @@ var http = require("http");
 const express        = require('express');
 const bodyParser     = require('body-parser');
 const app            = express();
-const port = 2222;
+const port = 1111;
 const answer = {imageUrl: "https://via.placeholder.com/350x150",
                 action: "https://bannerwise.io/",
-                buttonText: "Click Me"
-}
-
+                buttonText: "Click Me"};
 
 app.use(myParser.urlencoded({extended : true}));
 app.get("/yourpath", function(request, response) {
        console.log(JSON.stringify(answer)); //This prints the JSON document received
                                     // (if it is a JSON document)
 });
+
+var server = http.createServer(handleRequest);
+server.listen(port, function(){
+    //validate that it is running on the server
+    console.log("Server listening on: http://localhost:%s", port);
+});
+
+function handleRequest(request, response){
+    response.setHeader('Access-Control-Allow-Origin', '*');
+    response.setHeader('Access-Control-Allow-Headers',  'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+    response.setHeader('Content-Type', 'text/html; charset=UTF-8');
+    response.end(JSON.stringify(answer));
+}
 
