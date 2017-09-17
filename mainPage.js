f8beecd2100a3430f09b5e755b2f7512d1e4f090
LuisGonzaga <luis.caldas.costa@gmail.com>
2017-09-17 17:34:19 +0100

    updt

--- mainPage.js
+++ mainPage.js
@@ -1,28 +1,5 @@
 <script type="text/javascript">
-var Log = function(dataLog){
-	var d = new Date();
-	logFile.push(d.toUTCString()+dataLog);
-}
+funtion Test(){
+	alert("wot")};
 
-// 	function Timer(arg) {
-// 		var copy = [];
-// 		arg.logFile.forEach(function(item){
-// 		copy.push(item)
-// 		});
-// 		console.log(copy);
-//   }
-var $counter = 0;
-$("#target").mousedown(function() {
-		  $counter++;
-		  $("p").text(" Clicks: "+$counter);
-		  Log("Clicks " + $counter);
-});
-
-$("div").mousemove(function( event ) {
-	  var pageCoords = "( " + event.pageX + ", " + event.pageY + " )";
-	  $( "span" ).text( "(X, Y) : " + pageCoords );
-	  //logFile.push(d.toUTCString()+"(X, Y) : " + pageCoords);
-	  
-});	
 </script>
-  
