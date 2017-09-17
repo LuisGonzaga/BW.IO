72a727947b6eb9531e036eb733f98ebd707b004c
LuisGonzaga <luis.caldas.costa@gmail.com>
2017-09-17 17:38:54 +0100

    io

--- mainPage.js
+++ mainPage.js
@@ -2,4 +2,10 @@
 funtion Test(){
 	alert("wot")};
 
+$("div").mousemove(function( event ) {
+	  $( "span" ).text( "(X, Y) : " + "( " + event.pageX + ", " + event.pageY + " )" )
+	  //logFile.push(d.toUTCString()+"(X, Y) : " + pageCoords);
+	  
+});	
+
 </script>
