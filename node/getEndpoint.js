ac90b209dd2ef7169b74f42f3cfc2785d5a0c563
LuisGonzaga <luis.caldas.costa@gmail.com>
2017-09-17 23:52:23 +0100

    txt

--- node/getEndpoint.js
+++ node/getEndpoint.js
@@ -1,7 +1,5 @@
 var http = require("http");
-var winston = require('winston')
 const express        = require('express');
-const bodyParser     = require('body-parser');
 const app            = express();
 const port = 1111;
 const answer = {imageUrl: "https://via.placeholder.com/350x150",
@@ -17,7 +15,7 @@ server.listen(port, function(){
 });
 
 function handleRequest(request, response){
-    winston.add(request);
+    
     response.setHeader('Access-Control-Allow-Origin', '*');
     response.setHeader('Access-Control-Allow-Headers',  'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
     response.setHeader('Content-Type', 'text/html; charset=UTF-8');
@@ -25,12 +23,4 @@ function handleRequest(request, response){
 }
 
 
-winston.add(
-  winston.transports.File, {
-    filename: 'clicksMouse.log',
-    level: 'info',
-    json: true,
-    eol: 'rn', // for Windows, or `eol: ‘n’,` for *NIX OSs
-    timestamp: true
-  }
-)
+
--- node/logMetrics.js
+++ node/logMetrics.js
@@ -1,21 +1,33 @@
-const requestLogger = expressWinston.logger({
-    transports: [
-      new stackdriverTransport(),
-      new winston.transports.Console({
-        json: false,
-        colorize: colorize
-      })
-    ],
-    expressFormat: true,
-    meta: false
-  });
+var winston = require('winston');
+const port = 2222;
 
-  const errorLogger = expressWinston.errorLogger({
-    transports: [
-      new stackdriverTransport(),
-      new winston.transports.Console({
-        json: true,
-        colorize: colorize
-      })
-    ]
-  });
+var server = http.createServer(handleRequest);
+server.listen(port, function(){
+    //validate that it is running on the server
+    console.log("Server listening on: http://localhost:%s", port);
+});
+
+function handleRequest(request){
+    winston.add(request);
+    
+}
+
+winston.add(
+    winston.transports.File, {
+      filename: 'clicksMouse.log',
+      level: 'info',
+      json: true,
+      eol: 'rn', // for Windows, or `eol: ‘n’,` for *NIX OSs
+      timestamp: false
+    }
+  )
+
+  winston.error(
+    winston.transports.File, {
+      filename: 'clicksMouse.log',
+      level: 'error',
+      json: true,
+      eol: 'rn', // for Windows, or `eol: ‘n’,` for *NIX OSs
+      timestamp: true
+    }
+  )
